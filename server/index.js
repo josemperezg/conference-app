eaf6aaf61a7a9c786b00a9092ceadecde42fc284
Jose Perez <josemperezg@gmail.com>
2020-11-05 08:30:28 -0600

    Message changed

--- server/index.js
+++ server/index.js
@@ -43,7 +43,7 @@ app.use('/', routes({
   feedback,
 }));
 
-app.use((req, res, next) => next(createError(404, 'File not found')));
+app.use((req, res, next) => next(createError(404, 'File not founded')));
 
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
